aebf6e02f7c0e2d5e30eab734c02a9b62f320e5f
James <james@iMac.fritz.box>
2025-12-05 13:43:01 +0000

    Randomise which images and question are shown

--- src/components/HabitatRanking.jsx
+++ src/components/HabitatRanking.jsx
@@ -2,16 +2,18 @@ import React, { useState, useEffect }  from "react";
 import { supabase } from "../lib/supabaseClient";
 
 function HabitatRanking({personId}) {
-
+    const HOLDING_IMAGE = "./images/holding.png"
     const [error, setError] = useState(null);
     const [rankHabitat, setRankHabitat] = useState(null);
-    const [loading, setLoading] = useState(true);
+
+    const [habitatImage1, setHabitatImage1] = useState(HOLDING_IMAGE);
+    const [habitatImage2, setHabitatImage2] = useState(HOLDING_IMAGE);
+    const [question, setQuestion] = useState('');
 
     useEffect(() => {
         let cancelled = false;
 
         async function loadNextPair(pairSubmission) {
-            setLoading(true);
             setError(null);
 
             try {
@@ -25,8 +27,11 @@ function HabitatRanking({personId}) {
 
                 const { nextQuestion, nextHabitat1, nextHabitat2 } = data ?? {};
 
-                if (!nextQuestion || !nextHabitat1 || !nextHabitat2) {
-                    throw new Error(`Missing variables for next pair ${JSON.stringify(data)}`);
+                if (!nextQuestion) {
+                    throw new Error(`Missing question for next pair ${JSON.stringify(data)}`);
+                }
+                if (isNaN(+nextHabitat1) || isNaN(+nextHabitat1)) {
+                    throw new Error(`Missing habitat for next pair ${JSON.stringify(data)}`);
                 }
 
                 if (!cancelled) {
@@ -37,8 +42,6 @@ function HabitatRanking({personId}) {
                     setError(err?.message ?? "Unknown error");
                     setRankHabitat(null);
                 }
-            } finally {
-                if (!cancelled) setLoading(false);
             }
         }
 
@@ -48,6 +51,34 @@ function HabitatRanking({personId}) {
             cancelled = true;
         };
     }, [personId]);
+
+    function imageUrl(clusterId) {
+        const images = [1,2,3,4,5,6,7,8,9,10]
+        const randomImageIndex = Math.floor(Math.random() * images.length);
+        return `./images/habitat/${clusterId}/${images[randomImageIndex]}.jpg`
+    }
+
+    useEffect(() => {
+        if (rankHabitat) {
+            setHabitatImage1(imageUrl(rankHabitat.nextHabitat1))
+            setHabitatImage2(imageUrl(rankHabitat.nextHabitat2))
+
+            switch (rankHabitat.nextQuestion) {
+                case 'biodiversity':
+                    setQuestion('Click on the image showing habitat that seems like it would support more kinds of wildlife (birds, pollinators, plants):')
+                    break;
+                case 'safety':
+                    setQuestion('Click on the image containing space would you feel safer spending time in—including at dusk:')
+                    break;
+                case 'living':
+                    setQuestion('Click on the image which would make you more likely to buy a home in the area:')
+                    break;
+                default:
+                    setQuestion('')
+                    setError(`Invalid question key ${rankHabitat.nextQuestion}`)
+            }
+        }
+    }, [rankHabitat]);
 
     return (
         <div className="habitat-ranking card-body p-4">
@@ -59,20 +90,19 @@ function HabitatRanking({personId}) {
             ) : (
                 <>
                     <p className="mb-4">
-                        Click the photo in which space seems like it would support more kinds of wildlife
-                        (birds, pollinators, plants)?
+                        {question}
                     </p>
 
                     <img
                         alt="Habitat image to compare A"
                         className="habitat-ranking card-img"
-                        src="./images/habitat/1/1.jpg"
+                        src={habitatImage1}
                     />
                     <hr />
                     <img
                         alt="Habitat image to compare B"
                         className="habitat-ranking card-img"
-                        src="./images/habitat/2/1.jpg"
+                        src={habitatImage2}
                     />
                 </>
             )}
