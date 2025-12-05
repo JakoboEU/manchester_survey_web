9792d6476160819064beff545966ea086c4af5a2
James <james@iMac.fritz.box>
2025-12-05 12:13:13 +0000

    Update development vars

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.REMOTE_URL;
-const supabaseAnonKey = import.meta.env.REMOTE_KEY;
+const supabaseUrl = import.meta.env.VITE_REMOTE_URL;
+const supabaseAnonKey = import.meta.env.VITE_REMOTE_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error("Missing Supabase environment variables");
