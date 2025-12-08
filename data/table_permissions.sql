590bf8116674c49c57e5718f6456b6c9ed9f82b0
James <james@iMac.fritz.box>
2025-12-08 12:14:46 +0000

    Add table permissions SQL

---
+++ data/table_permissions.sql
@@ -0,0 +1,48 @@
+alter table person enable row level security;
+
+create policy "Person can be selected by everyone."
+    on person for select
+    to authenticated
+    using ( true );
+
+create policy "Person can be inserted by everyone."
+    on person for insert
+    with check (
+      auth.role() = 'authenticated'
+      and auth.jwt()->>'is_anonymous' = 'true'
+    );
+
+alter table demographics enable row level security;
+
+create policy "Demographics can be selected by everyone."
+   on demographics for select
+   to authenticated
+   using ( true );
+
+alter table person_demographics enable row level security;
+
+create policy "Demographics can be selected by everyone."
+    on person_demographics for select
+    to authenticated
+    using ( true );
+
+create policy "Demographics can be insected by everyone."
+    on person_demographics for insert
+    with check (
+      auth.role() = 'authenticated'
+      and auth.jwt()->>'is_anonymous' = 'true'
+    );
+
+alter table habitat enable row level security;
+
+create policy "Habitat can be selected by everyone."
+    on habitat for select
+    to authenticated
+    using ( true );
+
+create policy "Habitat can be updated by everyone."
+    on habitat for update
+    with check (
+        auth.role() = 'authenticated'
+        and auth.jwt()->>'is_anonymous' = 'true'
+    );
