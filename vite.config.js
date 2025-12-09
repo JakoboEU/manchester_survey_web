50f12382236245091b5169bcc38cb1b0decb2e55
James <james@iMac.fritz.box>
2025-12-09 19:12:27 +0000

    Change path to be root - setting up for domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/manchester_survey_web/",
+    base: "/",
     build: {
         outDir: "docs",
     },
